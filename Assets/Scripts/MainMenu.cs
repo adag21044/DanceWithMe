a122d6c5599b050ef77edef1be9ba828e4cdab67
adag21044 <90869456+adag21044@users.noreply.github.com>
2022-12-30 15:10:20 +0300

    try to add button
    try to add button

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -2,24 +2,37 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 public class MainMenu : MonoBehaviour
 {
+    /*Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);  
+      RaycastHit hit;  
+      if (Physics.Raycast(ray, out hit)) {  
+        //Select stage    
+        if (hit.transform.name == "Play Button") {  
+          SceneManager.LoadScene("Shop"); */
     public GameData gameData;
+    public Button button;
     private void Awake()
     {
         gameData = SaveSystem.Load();
+        
     }
     public void Update()
     {
-        if(Input.GetMouseButton(0))
+   
+        if(Input.GetButtonDown("button"))
         {
             PlayGame();
         }
         else
-        if(Input.touchCount > 0 )
+        if(Input.GetMouseButton(1))
         {
-            PlayGame();
+            ShopMenu();
+            Debug.Log("Shop");
         }
+       
+        
     }
     public void PlayGame()
     {
@@ -29,6 +42,6 @@ public class MainMenu : MonoBehaviour
 
     public void ShopMenu()
     {
-        SceneManager.LoadScene("Shop");
+        SceneManager.LoadScene("Shop menu");
     }
 }
